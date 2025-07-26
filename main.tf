b6d489fc3e0e72b691f56f2a52a41e8efec6feaa
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-07-26 22:44:50 +0200

    Update provider

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         extrm-fabric-engine = {
             source = "tchevalleraud/extrm-fabric-engine"
-            version = "~> 0.0.6"
+            version = "~> 0.0.7"
         }
     }
 }
