dbe951e941edff8781c80e05f5a96f7a29f3a0c0
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-07-26 21:02:30 +0200

    20250726-2102 - init terraform

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_providers {
+        extrm-fabric-engine = {
+            source = "tchevalleraud/extrm-fabric-engine"
+            version = "~> 0.0.1"
+        }
+    }
+}
+
+provider "extrm-fabric-engine" {
+    alias       = "vVOSS1"
+    host        = var.vvoss1_host
+    port        = 22
+    username    = var.vvoss1_user
+    password    = var.vvoss1_pass
+}
+
+resource "extrm-fabric-engine_hostname" "sw1" {
+    hostname    = var.vvoss1_name
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "device1_host" {}
+variable "device1_user" {}
+variable "device1_pass" {
+  sensitive = true
+}
+variable "device1_name" {}
